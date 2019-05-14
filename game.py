61a44646233488cb9683ef9edbca90dc7dd7108f
Rohith Sudheer <36558130+RohithS98@users.noreply.github.com>
2019-05-14 09:10:10 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,143 @@
+import pygame
+from random import randint
+from player import Player
+
+class Platform:
+    def __init__(self, px, py, width, height):
+        self.px = px
+        self.py = py
+        self.width = width
+        self.height = height
+        self.rect = pygame.Rect(px,py,width,height)
+    
+    def getDrawBoxTopLeft(self):
+        return self.px - self.width/2, self.py - self.height/2
+
+    def getCenter(self):
+        return self.px, self.py
+
+    def getDim(self):
+        return self.width, self.height
+
+    def setDim(self,dim):
+        self.width, self.height = dim
+
+    def getRect(self):
+        return self.rect
+
+
+FPS = 60
+PLAYER_WIDTH = 56
+PLAYER_HEIGHT = 40
+PLAYER_JUMP = -8.2
+GRAVITY = 1*30.0/FPS
+PLAYER_STARTX = 220
+PLAYER_STARTY = 320
+PLAYER_MAX_Y = 630
+
+class Game:
+
+    def __init__(self):
+        pygame.init()
+        self.gameStart = False
+        self.screen = pygame.display.set_mode((480,640))
+        self.clock = pygame.time.Clock()
+        self.spriteSheet = {}
+
+    @staticmethod
+    def isVisible(coord,dim):
+        if 0 <= coord[0] - dim[0]/2 <= 480:
+            return True
+        if 0 <= coord[0] + dim[0]/2 <= 480:
+            return True
+        return False
+
+    def loadSprite(self, t):
+        if t not in self.spriteSheet:
+            temp = pygame.image.load(t).convert_alpha()
+            mask = pygame.mask.from_surface(temp)
+            self.spriteSheet[t] = temp, mask
+
+    def start(self):
+        self.inGame = True
+        self.player = Player(gravity=GRAVITY, upVel = PLAYER_JUMP,
+                                         px = PLAYER_STARTX, py = PLAYER_STARTY,
+                                         maxy = PLAYER_MAX_Y)
+        self.boxList = []
+        self.stageElementList = []
+        self.stageElementList.append(Platform(60,490,100,30,))
+
+    def drawPlatform(self,platform):
+        pygame.draw.rect(self.screen,(0,0,0), platform.getRect())
+
+    def drawPlayer(self):
+        t = self.player.getSprite()
+        self.loadSprite(t)
+        cord = list(self.player.getCenter())
+        temp = self.spriteSheet[t][0]
+        box = temp.get_rect().width, temp.get_rect().height
+        cord[0] = cord[0] - box[0]/2
+        cord[1] = cord[1] - box[1]/2
+        self.screen.blit(temp,cord)
+
+    def draw(self):
+        self.screen.fill((255,255,255))
+        for i in self.stageElementList:
+            self.drawPlatform(i)
+        self.drawPlayer()
+        pygame.display.flip()
+
+    def wait(self,fps):
+        self.clock.tick(fps)
+
+    def destroy(self):
+        pygame.quit()
+
+    def isRunning(self):
+        return self.inGame
+
+    def update(self):
+        if self.gameStart:
+            self.player.update(self.stageElementList)
+
+    def startGame(self):
+        self.gameStart = True
+
+    def getInput(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.inGame = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                self.player.jump()
+            if not self.gameStart:
+                self.startGame()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.player.moveX(-1)
+        if keys[pygame.K_RIGHT]:
+            self.player.moveX(1)
+
+    def checkSingleCollision(self, player, platform):
+        return player.getRect().colliderect(platform.getRect())
+
+    def checkCollision(self):
+        self.playerStageColl = []
+        for i in self.stageElementList:
+            if self.checkSingleCollision(self.player,i):
+                self.playerStageColl.append(i)
+
+    def dead(self):
+        self.gameStart = False
+        self.start()
+
+game = Game()
+game.start()
+#game.startGame()
+while game.isRunning():
+    game.getInput()
+    game.update()
+    
+    game.draw()
+    game.wait(FPS)
+
+game.destroy()
---
+++ player.py
@@ -0,0 +1,87 @@
+import math
+from pygame import Rect
+
+class Player:
+    def __init__(self, px = 100, py = 250, width = 56, height = 40, maxy = 800, gravity = 0, upVel = -7, maxVel = 20):
+        self.px = px
+        self.py = py
+        self.gravity = gravity
+        self.xvel = 0
+        self.yvel = 0
+        self.width = width
+        self.height = height
+        self.upVel = upVel
+        self.maxVel = maxVel
+        self.score = 0
+        self.xVelMax = 5
+        self.maxy = maxy
+        self.xMove = False
+        self.xdel = width/5
+        self.rect = Rect(0,0,0,0)
+        self.updateRect()
+
+    def updateRect(self):
+        self.rect.x, self.rect.y = self.getDrawBoxTopLeft()
+        self.rect.w, self.rect.h = self.getDim()
+
+    def getRect(self):
+        return self.rect
+
+    def getSprite(self):
+        return r"assets\Player.png"
+    
+    def getDrawBoxTopLeft(self):
+        return self.px - self.width/2, self.py - self.height/2
+
+    def getCenter(self):
+        return self.px, self.py
+
+    def getDim(self):
+        return self.width, self.height
+
+    def incX(self,dx,bl):
+        self.px += dx
+        self.updateRect()
+        for i in bl:
+            if self.getRect().colliderect(i.getRect()):
+                if dx >= 0:
+                    self.px = i.getRect().x
+                    self.vx = 0
+                else:
+                    self.px = i.getRect().x + i.getRect().w
+                break
+        self.updateRect()
+
+    def incY(self,dy,bl):
+        self.py += dy
+        self.updateRect()
+        for i in bl:
+            if self.getRect().colliderect(i.getRect()):
+                if dy >= 0:
+                    self.px = i.getRect().y
+                else:
+                    self.px = i.getRect().y + i.getRect().h
+                self.xvel = 0
+                break
+        self.updateRect()
+
+    def update(self, blockList):
+        self.incX(self.xvel,blockList)
+        self.incY(self.yvel,blockList)
+        self.py = min(self.py,self.maxy)
+        self.yvel = min(self.yvel+self.gravity, self.maxVel)
+        if not self.xMove:
+            self.xvel = 0
+        self.xMove = False
+        self.updateRect()
+
+    def moveX(self,direction):
+        self.xvel = self.xVelMax*direction
+        self.xMove = True
+
+    def jump(self):
+        self.yvel = self.upVel
+
+    def setDim(self,dim):
+        self.width, self.height = dim
+
